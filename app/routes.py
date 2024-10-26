7ee400c6cb420d503477b2d987107f74e5e2011c
akulewicz <a.kulewicz@gmail.com>
2024-10-26 14:23:11 +0200

    Add 404 page

--- app/routes.py
+++ app/routes.py
@@ -34,3 +34,8 @@ def home():
 @app.route("/onas")
 def about():
     return render_template("about.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
